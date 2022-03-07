3e8172443a4769ff33b99e8e2880e8430817a36a
BradyWalters <brady.walters5@gmail.com>
2022-03-07 15:59:32 -0800

    connects to channel, has a death counter only subs can activate

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const tmi = require('tmi.js')
 
 const opts = {
+    options: { debug: true },
     identity: {
         username: process.env.BOT_USERNAME,
         password: process.env.OAUTH_TOKEN
@@ -10,5 +12,24 @@ const opts = {
     ]
 };
 
+let deaths = 0
+
 const client = new tmi.client(opts)
 
+client.on('connected', onConnectedHandler)
+client.on('message', onMessageHandler)
+
+client.connect()
+
+function onConnectedHandler(addr, port) {
+    console.log(`* Connected to ${addr}:${port}`)
+}
+
+function onMessageHandler(channel, tags, message, self) {
+    if(self) return
+
+    if(message.toLowerCase() === '!death' && tags.subscriber) {
+        deaths++
+        client.say(channel, `Brady has died ${deaths} times, what a loser LUL`)
+    }
+}
